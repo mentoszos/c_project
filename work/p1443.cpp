de635834e58f139182013511e1ff7dc7e2cc27f7
doublemints <634748383@qq.com>
2022-04-06 23:28:49 +0800

    4.6

--- work/p1443.cpp
+++ work/p1443.cpp
@@ -4,13 +4,25 @@ int n,m,x,y;
 int map1[500][500];
 bool vis[500][500];
 int cnt=0;
-int step[][]={{-1,}}
+int step[][2]={{-2,-1},{2,-1},{-2,1},{2,1},{-1,-2},{-1,2},{1,2},{1,-2}};
 void go_from(int x,int y){
     if(x<1||x>n||y<1||y>m){
         return;
     }
     for(int i=0;i<8;i++){
-
+        x +=map1[i][0];
+        y +=map1[i][1];
+        if(!vis[x][y]){//如果能走
+            cnt++;
+            vis[x][y]=false;
+            map1[x][y] = cnt;
+            go_from(x,y);
+            cnt--;
+            vis[x][y]=true;
+            
+        }
+        x -=map1[i][0];
+        y -=map1[i][1];
     }
 }
 int main(){
