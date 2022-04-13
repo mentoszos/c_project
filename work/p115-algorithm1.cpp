23b3fe78ebc8b8e2300e63f4f222805fa522b976
mentos <634748383@qq.com>
2022-04-13 16:21:08 +0800

    4.13

---
+++ work/p115-algorithm1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun1(char str[],int index,int n){
+   if(index <0) return 0;
+   int temp = str[index]-48;//字符转数字
+   for(int i =0;i<n;i++){
+      temp*=10;
+   }
+   return temp+fun1(str,index-1,n+1);
+}
+int main(){
+   char str[100];
+   scanf("%s",str);
+   int i=0;
+   for(;i<100;i++){
+      if(str[i]=='\0') break;
+   }
+ printf("%d",fun1(str,i-1,0));
+}
