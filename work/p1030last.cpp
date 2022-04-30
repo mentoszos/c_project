c74885e4b01992d7cdf19ae4423a28d2752fe58f
mentos <634748383@qq.com>
2022-04-30 15:35:59 +0800

    4.30

--- work/p1030last.cpp
+++ work/p1030last.cpp
@@ -1,8 +1,8 @@
 //已知中序前序序求后序
 #include<bits/stdc++.h>
 using namespace std;
-char mid[20];
-char pre[20];
+char mid[50];
+char pre[50];
 void printPre(int l1,int r1,int l2,int r2){//先序中序的起点终点
     if(l1>r1) return;//遍历出界
     int i=l2;
@@ -16,6 +16,6 @@ void printPre(int l1,int r1,int l2,int r2){//先序中序的起点终点
 int main(){
     scanf("%s",mid);
     scanf("%s",pre);
-    printPre(0,sizeof(pre)-2,0,sizeof(mid)-2);
+    printPre(0,strlen(pre)-1,0,strlen(mid)-1);
     return 0;
 }
